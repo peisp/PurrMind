7424afe2f52db2a3c5c05100442134ea13ce39a1
peishaopeng <peishaopeng@gmail.com>
2025-05-17 13:07:24 +0800

    移除注释

--- src/components/ui/sidebar.jsx
+++ src/components/ui/sidebar.jsx
@@ -193,7 +193,6 @@ const Sidebar = React.forwardRef((
       data-collapsible={state === "collapsed" ? collapsible : ""}
       data-variant={variant}
       data-side={side}>
-      {/* This is what handles the sidebar gap on desktop */}
       <div
         className={cn(
           "relative w-[--sidebar-width] bg-transparent transition-[width] duration-200 ease-linear",
